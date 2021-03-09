4b03658f2fb95b12e20ff206db7f4903e2064f6c
Robert Mayer <rwlmayer@gmail.com>
2021-03-09 15:11:59 +0100

    Added sql connection from console

--- 2021-03-08/OP2WebApp/ConsoleApp1/Program.cs
+++ 2021-03-08/OP2WebApp/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string connectionString = "Database=demo;Server=.\sqlexpress;Integrated Security=SSPS";
+            string connectionString = "Database=demo;Server=.;Integrated Security=SSPI";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT P.[id], P.[name], P.[city] FROM [people] P";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"id: {reader.GetInt32(0)}, name: {reader.GetString(1)}, city: {reader.GetString(2)}");
+                        }
+                    }
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
